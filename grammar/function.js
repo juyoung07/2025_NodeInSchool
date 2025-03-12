f9fe5000fd93f9a090411785ea587f115105ab2d
juyoung07 <yunju5090@gmail.com>
2025-03-12 11:43:57 +0900

    arrow function 03

--- grammar/function.js
+++ grammar/function.js
@@ -3,8 +3,7 @@ function printString (a) {
 }
 printString('a');
 
-// 매개 변수가 하나인 경우 소괄호를 없얌
-anonymousFunction = a => {
-    console.log('anonymousFunction ' + a);
-}
+// 매개 변수가 하나인 경우 소괄호를 없애도 됨
+// 함수 body가 한 줄이거나 return문 하나일 때, 중괄호를 없애도 됨
+anonymousFunction = a => console.log('anonymousFunction ' + a);
 anonymousFunction('swag');
