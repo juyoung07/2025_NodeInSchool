b3733b9ce11129f09135bf579d62ccad2f18078a
juyoung07 <yunju5090@gmail.com>
2025-03-12 11:08:06 +0900

    function...

---
+++ grammar/function.js
@@ -0,0 +1,10 @@
+function printString (a) {
+    console.log('printString : ' + a);
+}
+printString('a');
+
+// 익명함수
+anonymousFunction = function (a) {
+    console.log('anonymousFunction ' + a);
+}
+anonymousFunction('swag');
