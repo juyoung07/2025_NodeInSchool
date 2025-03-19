948fa3c1e15c2dc3047e2363a8e011f1f559ab84
juyoung07 <yunju5090@gmail.com>
2025-03-19 12:03:01 +0900

    route 적용 전

--- app.js
+++ app.js
@@ -3,12 +3,20 @@ const app = express();
 
 app.use(express.json());
 
+app.get('/swag', (req, res) => {
+  res.status(200).send('get swag');
+});
+
+app.post('/swag', (req, res) => {
+  res.status(201).send('post swag');
+});
+
 app.get('/swag/:person', (req, res) => {
   const person = req.params.person;
   res.status(200).send(person);
-  });
+});
   
   // 서버 실행 (포트 3000)
-  app.listen(3000, () => {
-    console.log('서버가 http://localhost:3000 에서 실행 중입니다.');
-  });
+app.listen(3000, () => {
+  console.log('서버가 http://localhost:3000 에서 실행 중입니다.');
+});
