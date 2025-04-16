e799589b6ae10d5fb0ac189b7d57a174c0fa2fe9
juyoung07 <yunju5090@gmail.com>
2025-04-16 11:55:35 +0900

    createPool

--- database/database.js
+++ database/database.js
@@ -2,19 +2,11 @@ const mysql = require('mysql2');
 
 require('dotenv').config();
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
     host : process.env.DB_HOST,
     user : process.env.DB_USER,
     password : process.env.DB_PASSWORD,
     database : process.env.DB_NAME
 });
-  
-db.connect((err) => {
-    if (err) {
-      console.log('MySQL 연결 실패 : ', err);
-      return;
-    }
-    console.log('MySQL에 연결되었습니다.')
-})
 
 module.exports = db;
